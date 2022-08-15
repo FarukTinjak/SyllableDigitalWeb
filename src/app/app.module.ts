19ab4b9bcb53e51a3ff58f73c7194bb2795e3d27
FarukTinjak <fare_tc@hotmail.com>
2022-08-15 21:13:56 +0200

    Add header, banner, clients, mobile, about, contact and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    BannerComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutComponent,
+    MobileComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [HomeComponent,HeaderComponent,BannerComponent,ContactComponent,FooterComponent,AboutComponent,MobileComponent,ClientsComponent]
+
 })
 export class AppModule { }
