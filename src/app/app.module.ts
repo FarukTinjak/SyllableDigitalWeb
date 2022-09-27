796f6b63545c01f5b4b7ee49e09cc4c58056e977
FarukTinjak <fare_tc@hotmail.com>
2022-09-27 19:21:13 +0200

    Add page scroll package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { HomeComponent } from './home.component';
-import { HeaderComponent } from './header/header.component';
-import { BannerComponent } from './banner/banner.component';
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { MobileComponent } from './mobile/mobile.component';
-import { ClientsComponent } from './clients/clients.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {BannerComponent} from './banner/banner.component';
+import {ContactComponent} from './contact/contact.component';
+import {FooterComponent} from './footer/footer.component';
+import {MobileComponent} from './mobile/mobile.component';
+import {ClientsComponent} from './clients/clients.component';
+import {BlogComponent} from './blog/blog.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import {NgxPageScrollModule} from "ngx-page-scroll";
+import { PostComponent } from './post/post.component';
+
+const routes: Routes = [
+  {path: 'blog', component: BlogComponent},
+  {path: '', component: HomeComponent, pathMatch:"full"},
+  {path: 'blog/post/:id', component:PostComponent}
+];
+
 @NgModule({
   declarations: [
-    HomeComponent,
+    AppComponent,
+    BlogComponent,
     HeaderComponent,
-    BannerComponent,
-    ContactComponent,
-    FooterComponent,
-    AboutComponent,
-    MobileComponent,
-    ClientsComponent
+    BannerComponent, ContactComponent, FooterComponent, MobileComponent, ClientsComponent, HomeComponent, PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'}),
+    NgxPageScrollModule
   ],
-  providers: [],
-  bootstrap: [HomeComponent,HeaderComponent,BannerComponent,ContactComponent,FooterComponent,AboutComponent,MobileComponent,ClientsComponent]
+  providers: [
+  ],
+  bootstrap: [AppComponent,HeaderComponent,FooterComponent],
 
+  exports:
+    [RouterModule]
 })
-export class AppModule { }
+export class AppModule {
+}
