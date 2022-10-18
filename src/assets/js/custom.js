dbb7fb87281d3016453d0e92ca0e4c4092cfc5db
FarukTinjak <fare_tc@hotmail.com>
2022-10-18 18:31:53 +0200

    Add one page scroll navigation and hiding navbar on scroll

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -88,4 +88,54 @@ $(function () {
 	});
 
 
+  // Get all sections that have an ID defined
+  // Add an event listener listening for scroll
+  window.addEventListener("scroll", navHighlighter);
+  function navHighlighter() {
+    const sections = document.querySelectorAll("section[id]");
+    console.log(sections);
+    // Get current scroll position
+    let scrollY = window.pageYOffset;
+
+    // Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+      const sectionHeight = current.offsetHeight;
+
+      // Original:
+      // const sectionTop = current.offsetTop - 50;
+      //
+      // Alex Turnwall's update:
+      // Updated original line (above) to use getBoundingClientRect instead of offsetTop, based on:
+      // https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+      // https://newbedev.com/difference-between-getboundingclientrect-top-and-offsettop
+      // This allows the use of sections inside a relative parent, which I'm not using here, but needed for a project
+      //
+      const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+      var sectionId = current.getAttribute("id");
+        console.log(sectionId);
+
+      /*
+      - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+      - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+      */
+      if (
+        scrollY > sectionTop &&
+        scrollY <= sectionTop + sectionHeight
+      ){
+        document.querySelector(".navigation a[ng-reflect-href*=" + sectionId + "]").classList.add("active");
+      } else {
+        document.querySelector(".navigation a[ng-reflect-href*=" + sectionId + "]").classList.remove("active");
+      }
+
+      var a=document.querySelector(".navigation a.active");
+      if(a==null) {
+        $("header").css('opacity', '1');
+      }
+      else {
+        $("header").css('opacity', '0')
+
+      }
+    });
+  }
+
 });
